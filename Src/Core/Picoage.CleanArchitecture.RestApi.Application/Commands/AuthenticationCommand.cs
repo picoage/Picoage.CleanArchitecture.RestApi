266a094f6be7167e6b80e3cc20694b0f79499ec5
Vishnu Kumar <microsophtdeveloper@outlook.com>
2022-03-27 19:14:48 +0100

    command added

---
+++ Src/Core/Picoage.CleanArchitecture.RestApi.Application/Commands/AuthenticationCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Picoage.CleanArchitecture.RestApi.Application.Models.ResposeModels;
+using Picoage.CleanArchitecture.RestApi.Application.RequestModels;
+
+namespace Picoage.CleanArchitecture.RestApi.Application.Commands
+{
+    public class AuthenticationCommand: IRequest<AuthenticationResponse>
+    {
+        public AuthenticationCommand(AuthenticationRequest authenticationRequest)
+        {
+            AuthenticationRequest = authenticationRequest;
+        }
+
+        public AuthenticationRequest AuthenticationRequest { get; }
+    }
+}
