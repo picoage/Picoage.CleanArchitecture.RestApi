ec7348d350e7b91fcaefeed24e7e441702956f5a
Vishnu Udayakumar <micosophtdeveloper@outlook.com>
2025-07-13 23:55:28 +0100

    inti

--- Src/Core/Picoage.CleanArchitecture.RestApi.Application/Queries/AuthenticationQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-using Picoage.CleanArchitecture.RestApi.Application.Models.ResposeModels;
-using Picoage.CleanArchitecture.RestApi.Application.RequestModels;
-
-namespace Picoage.CleanArchitecture.RestApi.Application.Queries;
-public record AuthenticationQuery(AuthenticationRequest AuthenticationRequest) : IRequest<AuthenticationResponse>
-{
-
-}
